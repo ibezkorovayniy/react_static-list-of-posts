4d3193365790bc111bb26ef0e0dcabf2498f95ca
Illia Bezkorovainyi <illia.bezkorovainyi@heathmont.net>
2019-09-20 01:19:34 +0300

    Updates

---
+++ precache-manifest.f9d5a2509908ac8bf1e7e63491996770.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6065fdcbd0e9cb8f5c741c3495d64262",
+    "url": "/ibezkorovayniy/react_static-list-of-posts.git/index.html"
+  },
+  {
+    "revision": "619ad22a6bfecd8b31a4",
+    "url": "/ibezkorovayniy/react_static-list-of-posts.git/static/css/main.df8b8472.chunk.css"
+  },
+  {
+    "revision": "fa67027c564020748778",
+    "url": "/ibezkorovayniy/react_static-list-of-posts.git/static/js/2.78963138.chunk.js"
+  },
+  {
+    "revision": "619ad22a6bfecd8b31a4",
+    "url": "/ibezkorovayniy/react_static-list-of-posts.git/static/js/main.8b2cb146.chunk.js"
+  },
+  {
+    "revision": "ac96c5433c653072d0bc",
+    "url": "/ibezkorovayniy/react_static-list-of-posts.git/static/js/runtime~main.5ff9a0f4.js"
+  }
+]);
